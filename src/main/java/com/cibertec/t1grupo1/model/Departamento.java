aad169c44d9701d2368b94e06277954cc3afa49e
alemilahouse-max <alemilahouse@gmail.com>
2026-07-24 15:16:40 -0500

    Refactor Departamento class with annotations
    Added missing annotations for the 'nombre' field and updated the table name.

--- src/main/java/com/cibertec/t1grupo1/model/Departamento.java
+++ src/main/java/com/cibertec/t1grupo1/model/Departamento.java
@@ -2,20 +2,24 @@ package com.cibertec.t1grupo1.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter @NoArgsConstructor
+@Table(name = "departamento")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Departamento {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 60)
     private String nombre;
 
     @OneToMany(mappedBy = "departamento")
