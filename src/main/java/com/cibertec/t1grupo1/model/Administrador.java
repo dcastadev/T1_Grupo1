902e7fe1fe7c963a7bddd34db47d21a4fbc7d3a3
alemilahouse-max <alemilahouse@gmail.com>
2026-07-24 15:15:58 -0500

    Refactor Administrador class annotations and fields

--- src/main/java/com/cibertec/t1grupo1/model/Administrador.java
+++ src/main/java/com/cibertec/t1grupo1/model/Administrador.java
@@ -1,15 +1,25 @@
 package com.cibertec.t1grupo1.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Getter @Setter @NoArgsConstructor
+@Table(name = "administrador")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Administrador extends Persona {
 
+    @Column(nullable = false, unique = true, length = 40)
     private String usuario;
+
+    @Column(nullable = false, length = 120)
     private String password;
-    private String rol; // ej: "SUPERVISOR", "RRHH"
+
+    @Column(nullable = false, length = 30)
+    private String rol;
 }
