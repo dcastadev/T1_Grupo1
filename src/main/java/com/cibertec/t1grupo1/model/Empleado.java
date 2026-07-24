12644975c258a403c5fd4ebe1207164b17c3a9a2
alemilahouse-max <alemilahouse@gmail.com>
2026-07-24 15:16:55 -0500

    Update Empleado.java

--- src/main/java/com/cibertec/t1grupo1/model/Empleado.java
+++ src/main/java/com/cibertec/t1grupo1/model/Empleado.java
@@ -2,26 +2,33 @@ package com.cibertec.t1grupo1.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter @NoArgsConstructor
+@Table(name = "empleado")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Empleado extends Persona {
 
-    @Column(name = "fecha_ingreso")
+    @Column(
+            name = "fecha_ingreso",
+            nullable = false
+    )
     private LocalDate fechaIngreso;
 
-    // TODO (tema: Relaciones ManyToOne/OneToMany):
-    // confirmar fetch type y cascade adecuados aqui
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "departamento_id")
     private Departamento departamento;
 
-    @OneToMany(mappedBy = "empleado", cascade = CascadeType.ALL)
+    @OneToMany(
+            mappedBy = "empleado",
+            cascade = CascadeType.ALL
+    )
     private List<Asistencia> asistencias = new ArrayList<>();
 }
