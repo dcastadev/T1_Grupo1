01b5f7562756c1fb30b51a0510fafabe8ed8121b
alemilahouse-max <alemilahouse@gmail.com>
2026-07-24 15:17:33 -0500

    Refactor Persona class with additional annotations

--- src/main/java/com/cibertec/t1grupo1/model/Persona.java
+++ src/main/java/com/cibertec/t1grupo1/model/Persona.java
@@ -1,24 +1,38 @@
 package com.cibertec.t1grupo1.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "persona")
 @Inheritance(strategy = InheritanceType.JOINED)
-@Getter @Setter @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public abstract class Persona {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 80)
     private String nombres;
+
+    @Column(nullable = false, length = 80)
     private String apellidos;
 
-    @Column(unique = true, length = 8)
+    @Column(nullable = false, unique = true, length = 8)
     private String dni;
 
+    @Column(length = 120)
     private String email;
 }
