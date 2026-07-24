e27b2804aac2ea2609eb09c746bf3a0cc8375d8b
alemilahouse-max <alemilahouse@gmail.com>
2026-07-24 15:14:49 -0500

    Add findByUsuarioAndPassword method
    Added method to find Administrador by usuario and password.

--- src/main/java/com/cibertec/t1grupo1/repository/AdministradorRepository.java
+++ src/main/java/com/cibertec/t1grupo1/repository/AdministradorRepository.java
@@ -1,8 +1,17 @@
 // repository/AdministradorRepository.java
+
 package com.cibertec.t1grupo1.repository;
 
 import com.cibertec.t1grupo1.model.Administrador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
+import java.util.Optional;
+
+public interface AdministradorRepository
+        extends JpaRepository<Administrador, Long> {
+
+    Optional<Administrador> findByUsuarioAndPassword(
+            String usuario,
+            String password
+    );
 }
