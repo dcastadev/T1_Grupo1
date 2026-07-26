1d8ba50ae1edebbb4cae1b11ae7e0492ae1fc538
Edsson Santa Cruz <I202501385@CIBERTEC.EDU.PE>
2026-07-25 19:38:44 -0500

    Refina relaciones Departamento-Empleado y Empleado-Asistencia

--- src/main/java/com/cibertec/t1grupo1/model/Asistencia.java
+++ src/main/java/com/cibertec/t1grupo1/model/Asistencia.java
@@ -2,28 +2,34 @@ package com.cibertec.t1grupo1.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Entity
-@Getter @Setter @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class Asistencia {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "empleado_id", nullable = false)
     private Empleado empleado;
 
+    @Column(nullable = false)
     private LocalDate fecha;
+
     private LocalTime horaEntrada;
+
     private LocalTime horaSalida;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private EstadoAsistencia estado;
 }
